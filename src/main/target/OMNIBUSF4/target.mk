0d7c57db7d5d506616cff556d1231c6f99619448
DieHertz <diehertz@gmail.com>
2017-12-23 20:32:22 +0300

    Added KamikazeF4

--- src/main/target/OMNIBUSF4/target.mk
+++ src/main/target/OMNIBUSF4/target.mk
@@ -4,6 +4,7 @@ TARGET_SRC = \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/accgyro/accgyro_spi_icm20689.c \
             drivers/barometer/barometer_bmp085.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/barometer/barometer_ms5611.c \
